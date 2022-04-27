63fd3fabe1c6439a3c0a5e4f00447fa8d4cfcc79
lescata <alexandre.lescat@gmail.com>
2022-04-27 20:42:37 +0200

    First committ

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = 'Alan'
+let hacker2 = `Alex`
+console.log(`the driver name is ${hacker1}`)
+console.log(`the navigator name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
