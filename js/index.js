3396f53b8879e824d6be5ab1b40911b34bea64ec
lescata <alexandre.lescat@gmail.com>
2022-04-27 22:10:38 +0200

    3 exercice

--- js/index.js
+++ js/index.js
@@ -13,4 +13,31 @@ if (hacker1.length>hacker2.length) {
     console.log(`Wow, you both have equally long names, ${hacker2.length} characters!.`)
   }
 
-// Iteration 3: Loops
+// Iteration 3: loop
+const hacker = hacker1.toUpperCase()
+let result = ""
+let result2 = ""
+
+
+for(let i = 0;i<=hacker.length-1;i++) {
+ result += hacker[i]+" ";
+}
+console.log(result)
+
+for (let j =hacker.length-1 ; j >= 0;j--) {
+   result2 += hacker1[j]
+}
+console.log(result2)
+let k = 0
+while (hacker1[k]!=hacker2[k]) {
+  k++;
+}
+ let search =hacker1[k].localeCompare(hacker2[k])
+ if (search = 1) {
+   console.log("The driver's name goes first.")
+ }
+else if (search = -1){
+  console.log("Yo, the navigator goes first definitely.")
+}else {
+  console.log("What?! You both have the same name?")
+}
